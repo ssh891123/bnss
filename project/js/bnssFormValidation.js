4f87fb7c22736d0f6c7949347bd5066f5f290adc
SungHyun_Son <ssh891123@gmail.com>
2017-12-21 23:34:17 +0900

    폼 유효성 추가 핸드폰 번호 입력시 숫자만 받게 이메일 주소, 기존 라이브러리 삭제, 유효성 js파일 추가

---
+++ project/js/bnssFormValidation.js
@@ -0,0 +1,33 @@
+function validationEmail() {
+    var regex=/([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    var email = $('#findIdEmail').val();
+
+    if( email != 'undefined' && regex.test(email) )
+    {
+        document.getElementById("emailValidation").innerHTML = "";
+    }
+    else if( email == '')
+    {
+        document.getElementById("emailValidation").innerHTML = "email를 입력해 주십시오.";
+    }
+    else{
+        document.getElementById("emailValidation").innerHTML = "유효하지 않은 email 주소입니다.";
+    }
+}
+
+function onlyNumber(event){
+    event = event || window.event;
+    var keyID = (event.which) ? event.which : event.keyCode;
+    if ( (keyID >= 48 && keyID <= 57) || (keyID >= 96 && keyID <= 105) || keyID == 8 || keyID == 46 || keyID == 37 || keyID == 39 ) 
+        return;
+    else
+        return false;
+}
+function removeChar(event) {
+    event = event || window.event;
+    var keyID = (event.which) ? event.which : event.keyCode;
+    if ( keyID == 8 || keyID == 46 || keyID == 37 || keyID == 39 ) 
+        return;
+    else
+        event.target.value = event.target.value.replace(/[^0-9]/g, "");
+}
