2b7ca1e17bb1abf353aadf40741193cfde169881
SungHyun_Son <ssh891123@gmail.com>
2017-12-27 00:06:05 +0900

    로그인 수정

--- project/js/bnssUserJoin.js
+++ project/js/bnssUserJoin.js
@@ -1,68 +1,222 @@
-var REG_LOGIN = {
+var BNSS_USER_JOIN = {
+    
+    eventListener : function() {
+        $('#join_req').on('click', BNSS_USER_JOIN.request);
+        //$('#btn_idDupChk').on('click', BNSS_USER_JOIN.chkIdDup);
 
-    onCallBackForZipCd : function(){
-console.log('call back start');
-
-        new daum.Postcode({
-            oncomplete: function(data) {
-                // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
-
-                // 각 주소의 노출 규칙에 따라 주소를 조합한다.
-                // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
-                var fullAddr = ''; // 최종 주소 변수
-                var extraAddr = ''; // 조합형 주소 변수
-
-                // 사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
-                if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
-                    fullAddr = data.roadAddress;
-
-                } else { // 사용자가 지번 주소를 선택했을 경우(J)
-                    fullAddr = data.jibunAddress;
-                }
-
-                // 사용자가 선택한 주소가 도로명 타입일때 조합한다.
-                if(data.userSelectedType === 'R'){
-                    //법정동명이 있을 경우 추가한다.
-                    if(data.bname !== ''){
-                        extraAddr += data.bname;
-                    }
-                    // 건물명이 있을 경우 추가한다.
-                    if(data.buildingName !== ''){
-                        extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
-                    }
-                    // 조합형주소의 유무에 따라 양쪽에 괄호를 추가하여 최종 주소를 만든다.
-                    fullAddr += (extraAddr !== '' ? ' ('+ extraAddr +')' : '');
-                }
-
-                // 우편번호와 주소 정보를 해당 필드에 넣는다.
-                document.getElementById('sample6_postcode').value = data.zonecode; //5자리 새우편번호 사용
-                document.getElementById('sample6_address').value = fullAddr;
-
-                // 커서를 상세주소 필드로 이동한다.
-                document.getElementById('sample6_address2').focus();
-            }
-        }).open();
-      
-        
+        $('#join_userTelNo').addEventListener("input", myFunction);
+        // document.getElementById("join_userTelNo").addEventListener("input", myFunction);
     }
 
+
+
+    ,chkIdDup : function() {
+        console.log('asdfasdf');
+        var joinId = $('#join_userId').val();
+
+        //POST 방식 세팅
+        var form = document.createElement("form");
+        form.setAttribute("charset", "UTF-8");        
+        form.setAttribute("method", "post");        
+        form.setAttribute("action", "/userJoin/chkIdDup");        
+         
+        // ID
+        var hiddenField = document.createElement("input");        
+        hiddenField.setAttribute("type", "hidden");        
+        hiddenField.setAttribute("name", "joinId");        
+        hiddenField.setAttribute("value", joinId);        
+        form.appendChild(hiddenField);
+
+        document.body.appendChild(form);
+        var result = form.submit();
+    }
+
+    ,chkValidation : function() {
+        // ID Null Check
+        var id = $('#join_userId').val();
+        if( id == null || id == '') {
+            alert('ID를 입력해주세요.');
+            return false;
+        }
+
+        // 비밀번호 정합성 체크
+        var pw = $('#join_userPw').val();
+        var newPw = $('#join_userPwCnfm').val();
+
+        if( pw == null || pw == '' || newPw == null || newPw == '') {
+            alert('비밀번호를 입력해주세요.');
+            return false;
+        }
+
+        if( pw != newPw ) {
+            alert('비밀번호가 일치하지 않습니다.');
+            return false;
+        }
+
+        // EMAIL 정합성 체크
+        var email = $('#join_userEmail').val();
+        if( email == null || email == '') {
+            alert('E-Mail 주소를 입력해주세요.');
+            return false;
+        }
+
+        // 휴대폰 번호 Null 체크
+        var hpNo = $('#join_userTelNo').val();
+        if( hpNo == null || hpNo == '') {
+            alert('휴대폰 번호를 입력해주세요.');
+            return false;
+        }
+
+        // 이름 Null 체크
+        var nm = $('#join_userNm').val();
+        if( nm == null || nm == '') {
+            alert('이름을 입력해주세요.');
+            return false;
+        }
+
+        // 성별 Null 체크
+        var genderChk = !$('#join_genderIsM')[0].checked && !$('#join_genderIsF')[0].checked ? false : true ;
+        if( !genderChk ) {
+            alert('성별을 체크해주세요.');
+            return false;
+        }
+
+        // 생년월일 Null 체크
+        var birth = $('#join_userBirth').val();
+        if( birth == null || birth == '') {
+            alert('생년월일을 입력해주세요.');
+            return false;
+        }
+
+        // 교회 이름 Null 체크
+        var churchNm = $('#join_churchNm').val();
+        if( churchNm == null || churchNm == '') {
+            alert('교회 이름을 입력해주세요.');
+            return false;
+        }
+
+        // 교회 주소 Null 체크
+        var zipCd = $('#join_churchZipCd').val();
+        if( zipCd == null || zipCd == '') {
+            alert('우편번호를 입력해주세요.');
+            return false;
+        }
+
+        var srchAddr = $('#join_churchSrchAddr').val();
+        if( srchAddr == null || srchAddr == '') {
+            alert('검색주소를 입력해주세요.');
+            return false;
+        }
+
+        var detlAddr = $('#join_churchDetlAddr').val();
+        if( detlAddr == null || detlAddr == '') {
+            alert('상세주소를 입력해주세요.');
+            return false;
+        }
+
+        return true;
+    }
+
+    ,request : function() {
+        var joinId = $('#join_userId').val();
+        var joinPw = $('#join_userPw').val();
+        var joinEmail = $('#join_userEmail').val();
+        var joinTelNo = $('#join_userTelNo').val();
+        var joinNm = $('#join_userNm').val();
+        var joinGen = $('#join_genderIsM')[0].checked ? 'M' : 'F';
+        var joinBirth = $('#join_userBirth').val();
+        var joinChurchNm = $('#join_churchNm').val();
+        var joinChurchZipCd = $('#join_churchZipCd').val();
+        var joinChurchAddr = $('#join_churchSrchAddr').val() + ' ' + $('#join_churchDetlAddr').val();
+
+        //밸리데이션 체크
+        if( BNSS_USER_JOIN.chkValidation() ){
+            
+            //POST 방식 세팅
+            var form = document.createElement("form");
+            form.setAttribute("charset", "UTF-8");        
+            form.setAttribute("method", "post");        
+            form.setAttribute("action", "/userJoin");        
+            
+            // ID
+            var hiddenField = document.createElement("input");        
+            hiddenField.setAttribute("type", "hidden");        
+            hiddenField.setAttribute("name", "joinId");        
+            hiddenField.setAttribute("value", joinId);        
+            form.appendChild(hiddenField);
+            
+            // PW
+            hiddenField = document.createElement("input");        
+            hiddenField.setAttribute("type", "hidden");        
+            hiddenField.setAttribute("name", "joinPw");        
+            hiddenField.setAttribute("value", joinPw);        
+            form.appendChild(hiddenField);
+
+            // E-mail
+            hiddenField = document.createElement("input");        
+            hiddenField.setAttribute("type", "hidden");        
+            hiddenField.setAttribute("name", "joinEmail");        
+            hiddenField.setAttribute("value", joinEmail);        
+            form.appendChild(hiddenField);
+
+            // Phone No.
+            hiddenField = document.createElement("input");        
+            hiddenField.setAttribute("type", "hidden");        
+            hiddenField.setAttribute("name", "joinTelNo");        
+            hiddenField.setAttribute("value", joinTelNo);        
+            form.appendChild(hiddenField);
+                
+            // 이름
+            hiddenField = document.createElement("input");        
+            hiddenField.setAttribute("type", "hidden");        
+            hiddenField.setAttribute("name", "joinNm");        
+            hiddenField.setAttribute("value", joinNm);        
+            form.appendChild(hiddenField);
+
+            // 성별
+            hiddenField = document.createElement("input");        
+            hiddenField.setAttribute("type", "hidden");        
+            hiddenField.setAttribute("name", "joinGen");        
+            hiddenField.setAttribute("value", joinGen);        
+            form.appendChild(hiddenField);
+
+            // 생년월일
+            hiddenField = document.createElement("input");        
+            hiddenField.setAttribute("type", "hidden");        
+            hiddenField.setAttribute("name", "joinBirth");        
+            hiddenField.setAttribute("value", joinBirth);        
+            form.appendChild(hiddenField);
+
+            // 교회
+            hiddenField = document.createElement("input");        
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", "joinChurchNm");        
+            hiddenField.setAttribute("value", joinChurchNm);        
+            form.appendChild(hiddenField);
+            
+            // 교회 우편번호
+            hiddenField = document.createElement("input");        
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", "joinChurchZipCd");        
+            hiddenField.setAttribute("value", joinChurchZipCd);        
+            form.appendChild(hiddenField);
+
+            // 교회 상세주소
+            hiddenField = document.createElement("input");        
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", "joinChurchAddr");        
+            hiddenField.setAttribute("value", joinChurchAddr);        
+            form.appendChild(hiddenField);
+
+            document.body.appendChild(form);
+            var result = form.submit();
+        }        
+    }
 };
 
 
 $( document ).ready(function() {
-    console.log( "start" );
-
-    $('#test_1').on('click', function(){
-        REG_LOGIN.onCallBackForZipCd();
-    }) 
-      
-    
-      
-    
-    
-  
-       
-    
-
-    console.log("end");
+    console.log("User Join Page Load start");
+    BNSS_USER_JOIN.eventListener();
+    console.log("User Join Page Load End");
 });
